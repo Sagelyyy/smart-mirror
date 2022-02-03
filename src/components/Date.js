6e838ae1853f4803eac4fd09479676a408cc616d
Chris Weiskopf <orj.tsuk@gmail.com>
2022-02-02 20:04:52 -0500

    Major refactoring of date.js, completely removed switch statement and replaced it with some ternarys

--- src/components/Date.js
+++ src/components/Date.js
@@ -2,51 +2,19 @@ import React from "react";
 import moment from "moment";
 moment().format();
 
-export default class Timer extends React.Component {
+export default function Date() {
 
 
-  getDate() {
     const firstDate = "2016-01-15";
-    const years = moment(firstDate).fromNow(true);
-    const months = Math.floor(
-      moment.duration(moment().diff(moment(firstDate))).as("month") % 12
-    );
+    const [years, setYears] = React.useState(moment(firstDate).fromNow(true))
+    const [months, setMonths] = React.useState(
+      Math.floor( moment.duration(moment().diff(moment(firstDate))).as("month") % 12
+    ))
+    const [days, setDays] = React.useState(0)
+ 
+    console.log(Math.floor(moment().diff(firstDate, 'days')   / 24))
 
-    switch (months) {
-      default:
-        return <h2 className="date--text">{`Dating your babe-o for ${years} and counting...`}</h2>;
-      case 1:
-        return (
-          <h2 className="date--text">{`Dating your babe-o for ${years} and ${months} month and counting...`}</h2>
-        );
-      case 2:
-      case 3:
-      case 4:
-      case 5:
-      case 6:
-      case 7:
-      case 8:
-      case 9:
-      case 10:
-      case 11:
-      case 12:
-        return (
-          <h2 className="date--text">{`Dating your babe-o for ${years} and ${months} months and counting...`}</h2>
-        );
-    }
-  }
-
-  componentDidMount(){
-    this.getDate()
-    this.timerID = setInterval( () => 
-    this.getDate(), 86400000)
-}
-
-componentWillUnmount(){
-    clearInterval(this.timerID)
-}
-
-  render() {
-    return this.getDate();
-  }
+  return (
+    <h2 className="date--text">Together with your babe-o for {`${years}`} {months > 0 ? `and ${months} ${months === 1 ? "month" : "months"} and counting` : ""}</h2>
+  )
 }
