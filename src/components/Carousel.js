57ca68de710e4e457747317e2eeba3a3ad8a056b
Chris <orj.tsuk@gmail.com>
2022-02-11 08:07:06 -0500

    got the carousel functioning correctly.

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,71 +1,35 @@
 import { render } from "@testing-library/react";
 import React from "react";
-import memories from "../imgData"
+import memories from "../imgData";
 
+export default function Carousel() {
+  const photoMax = memories.length - 1;
+  let currPhoto = 0;
 
+  const [carouselImg, setCarouselimg] = React.useState(memories[0].src);
 
-
-export default function Carousel(){
-
-  const [carouselImg, setCarouselimg] = React.useState('')
-
-  function getRandomPhoto(){
-    const photoMax = memories.length
-    // return Math.floor(Math.random()*memories.length)
+  function getNextPhoto() {
+    if (currPhoto === photoMax) {
+      currPhoto = 0;
+      return memories[0].src;
+    }
+    currPhoto = currPhoto + 1;
+    return memories[currPhoto].src;
   }
 
   function refreshPhoto() {
-    setCarouselimg(memories[getRandomPhoto()].src,);
+    setCarouselimg(getNextPhoto());
   }
 
-      React.useEffect(() => {
-        //timer set to refresh every 10 sec
-        const timerId = setInterval(refreshPhoto, 10000);
-        return function cleanup() {
-          clearInterval(timerId);
-        };
-      }, []);
+  React.useEffect(() => {
+    //timer set to refresh every 10 sec
+    const timerId = setInterval(refreshPhoto, 10000);
+    return function cleanup() {
+      clearInterval(timerId);
+    };
+  }, []);
 
-  return(
-    <img className={`carousel--photo`} alt="babe-o-pic" src={carouselImg}/>
-  )
+  return (
+    <img className={`carousel--photo`} alt="babe-o-pic" src={carouselImg} />
+  );
 }
-
-// export default class Carousel extends React.Component{
-
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             src: memories,
-//             currentImg: memories[this.getRandomPhoto()].src,
-//         };
-//       }
-
-//       componentDidMount(){
-//         this.timerID = setInterval(
-//             () => this.tick(),
-//             10000
-//           );
-//       }
-
-//       componentWillUnmount(){
-//         clearInterval(this.timerID);
-//       }
-
-//       getRandomPhoto(){
-//         return Math.floor(Math.random()* memories.length)
-//     }
-
-//       tick() {
-//         this.setState({
-//           currentImg: memories[this.getRandomPhoto()].src,
-//         });
-//       }
-
-//     render(){
-//         return(
-//         <img className={`carousel--photo`} alt="babe-o-pic" src={this.state.currentImg}/>
-//         )
-//     }
-    
-// }
