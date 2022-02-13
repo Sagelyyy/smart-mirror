ef0eb919473b6f44d22bbacddf1938326c95fb12
Chris <orj.tsuk@gmail.com>
2022-02-13 00:19:22 -0500

    wrote thetime component over using hooks. cleaned up the code so it functioned better.

--- src/components/Time.js
+++ src/components/Time.js
@@ -2,51 +2,51 @@ import React from "react";
 import moment from "moment";
 moment().format();
 
-export default class Time extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
+export default function Time() {
+
+  const [clock, setClock] = React.useState({
       time: moment().format("hh:mm:ss a"),
       day: moment().format("dddd"),
       date: moment().format("LL"),
-    };
-  }
+  })
 
-  componentDidMount() {
-    this.timerID = setInterval(() => this.tick(), 1000);
-  }
+  const [greeting, setGreeting] = React.useState('')
 
-  componentWillUnmount() {
-    clearInterval(this.timerID);
-  }
-
-  tick() {
-    this.setState({
+    function tick() {
+      getGreeting()
+      setClock({
       time: moment().format("hh:mm:ss a"),
+      day: moment().format("dddd"),
+      date: moment().format("LL"),
     });
   }
 
-  greeting() {
+  function getGreeting() {
     if (moment().format("a") === "am") {
-      return "Morning";
+      setGreeting("Morning");
     } else if (
       moment().format("a") === "pm" &&
       moment().format("hh") > 1 &&
       moment().format("hh") <= 3
     ) {
-      return "Afternoon";
+      setGreeting("Afternoon");
     }
-    return "Evening";
+    setGreeting("Evening");
   }
 
-  render() {
-    return (
-      <div className="title">
-        <h1 className="title--greeting">{`Good ${this.greeting()} Babe-o!`}</h1>
-        <h3 className="title--day">{`${this.state.day}`}</h3>
-        <h3 className="title--day"> {`${this.state.date}`}</h3>
-        <h2 className="title--time">{`${this.state.time}`}</h2>
-      </div>
-    );
-  }
+  React.useState(() => {
+    const interval = setInterval(() => {
+      tick()
+    }, 1000)
+    return () => clearInterval(interval)
+  }, [])
+
+  return (
+    <div className="title">
+      <h1 className="title--greeting">{`Good ${greeting} Babe-o!`}</h1>
+      <h3 className="title--day">{`${clock.day}`}</h3>
+      <h3 className="title--day"> {`${clock.date}`}</h3>
+      <h2 className="title--time">{`${clock.time}`}</h2>
+    </div>
+  );
 }
